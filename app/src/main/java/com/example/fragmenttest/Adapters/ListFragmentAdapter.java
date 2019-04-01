bc61fc677238c4b1acf8a7b4333094d22a6f18dd
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-04-02 01:54:34 +0200

    Functional Ticket CRUD complete

--- app/src/main/java/com/example/fragmenttest/Adapters/ListFragmentAdapter.java
+++ app/src/main/java/com/example/fragmenttest/Adapters/ListFragmentAdapter.java
@@ -3,11 +3,13 @@ package com.example.fragmenttest.Adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.fragmenttest.Interface.ItemClickListener;
 import com.example.fragmenttest.R;
 import com.example.fragmenttest.objects.Ticket;
 
@@ -17,6 +19,7 @@ public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapte
 
     private Context context;
     private ArrayList<Ticket> ticketList;
+    private ItemClickListener itemClickListener;
 
     public ListFragmentAdapter(Context mContext, ArrayList<Ticket> data){
         this.context = mContext;
@@ -31,13 +34,31 @@ public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int i) {
+
+        final Ticket ticketData = ticketList.get(i);
+
         myViewHolder.tv_name.setText(ticketList.get(i).getName());
         myViewHolder.tv_category.setText(ticketList.get(i).getCategory());
         myViewHolder.tv_ticketType.setText(ticketList.get(i).getTicketType()+" ");
         myViewHolder.tv_currency.setText(String.valueOf(ticketList.get(i).getCurrency()));
         myViewHolder.tv_currencyType.setText(ticketList.get(i).getCurrencyType());
         myViewHolder.tv_date.setText(String.valueOf(ticketList.get(i).getDate()));
+
+        myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemClickListener.OnItemClick(i, ticketData);
+            }
+        });
+
+        myViewHolder.tv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ticketList.remove(i);
+                notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -47,7 +68,7 @@ public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView tv_name, tv_category, tv_ticketType, tv_currency, tv_currencyType, tv_date;
+        private TextView tv_name, tv_category, tv_ticketType, tv_currency, tv_currencyType, tv_date, tv_delete;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,6 +79,19 @@ public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapte
             tv_currency = (TextView) itemView.findViewById(R.id.custom_ticket_currency);
             tv_currencyType = (TextView) itemView.findViewById(R.id.custom_ticket_currency_type);
             tv_date = (TextView) itemView.findViewById(R.id.custom_ticket_date);
+            tv_delete = (TextView) itemView.findViewById(R.id.custom_ticket_tv_delete_item);
         }
+    }
+
+    public void setOnItemClickListener(ItemClickListener itemClickListener){
+        this.itemClickListener = itemClickListener;
+    }
+
+    public void UpdateData(int position, Ticket ticketData){
+
+        ticketList.remove(position);
+        ticketList.add(ticketData);
+        notifyItemChanged(position);
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/fragmenttest/Fragments/ListFragment.java
+++ app/src/main/java/com/example/fragmenttest/Fragments/ListFragment.java
@@ -8,7 +8,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,11 +22,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.fragmenttest.Adapters.ListFragmentAdapter;
 import com.example.fragmenttest.AddTicketActivity;
 import com.example.fragmenttest.Database.DatabaseHelper;
-import com.example.fragmenttest.HomeActivity;
+import com.example.fragmenttest.Interface.ItemClickListener;
 import com.example.fragmenttest.R;
 import com.example.fragmenttest.objects.Ticket;
 
@@ -32,13 +36,14 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 
-public class ListFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class ListFragment extends Fragment {
 
     private View v;
     private Context mContext;
     private RecyclerView recyclerView;
     private ListFragmentAdapter listFragmentAdapter;
     private ArrayList<Ticket> ticketArrayList;
+    private ArrayList<Ticket> filteredTicketArrayList;
     private ProgressDialog progressDialog;
 
     private String timeStamp;
@@ -46,9 +51,15 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
     private ArrayList<String> categoriesList;
     ArrayAdapter<String> adapter1;
     private Button btn_addTicket;
-    private EditText filterText;
+
+    private EditText et_filterText;
+    private String filterText = "";
 
     private Spinner categoryFilter;
+    private String categoryFilterText = "";
+
+    AlertDialog.Builder builder;
+    AlertDialog dialog;
 
     private DatabaseHelper db;
 
@@ -73,15 +84,16 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
         ticketArrayList.add(new Ticket(1,"Food Delux","Food","-",26.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Max","Food","-",6.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Me","House","-",46.10,"€",timeStamp));
-        ticketArrayList.add(new Ticket(1,"Food Hello","Food","-",999.50,"€",timeStamp));
+        ticketArrayList.add(new Ticket(1,"Food Hello","Food","-",999.50,"€","2019-10-26 06:05:00"));
         ticketArrayList.add(new Ticket(1,"Food Delux","House","-",2699.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Max","Food","-",99995.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Me","Food","-",46.10,"€",timeStamp));
-        ticketArrayList.add(new Ticket(1,"Food Hello","Electronics","-",2.50,"€",timeStamp));
+        ticketArrayList.add(new Ticket(1,"Food Hello","Electronics","-",2.50,"€","2019-08-10 06:05:00"));
         ticketArrayList.add(new Ticket(1,"Food Delux","Food","-",26.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Max","Food","-",6.50,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Me","Electronics","-",46.10,"€",timeStamp));
         ticketArrayList.add(new Ticket(1,"Food Hello","Bank","-",2.50,"€",timeStamp));
+
     }
 
     @Nullable
@@ -93,13 +105,10 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
         recyclerView = (RecyclerView) v.findViewById(R.id.recylerView_ticket_list);
         btn_addTicket = (Button) v.findViewById(R.id.button_add_ticket);
         categoryFilter = (Spinner) v.findViewById(R.id.spinner_category_filter);
-        filterText = (EditText) v.findViewById(R.id.editText_searchText);
-
-        listFragmentAdapter = new ListFragmentAdapter(getContext(), ticketArrayList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(listFragmentAdapter);
+        et_filterText = (EditText) v.findViewById(R.id.editText_searchText);
 
         adapter1 = new ArrayAdapter<>(mContext, android.R.layout.simple_spinner_dropdown_item, categoriesList);
+
 
         btn_addTicket.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,25 +128,90 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
         categoriesList = new ArrayList<>();
         getCategories();
         adapter1 = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, categoriesList);
-        categoryFilter.setOnItemSelectedListener(this);
         categoryFilter.setAdapter(adapter1);
-    }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        //Filter By Category
-        if (!adapterView.getSelectedItem().toString().equals("Filter by category ???")) {
-            // i am resetting the initial list of tickets with the filter one which is not what i wanted !!!!!!!!!!!!
-            ticketArrayList = filterByCategory(adapterView.getSelectedItem().toString());
-            Log.e("Category Select ", adapterView.getSelectedItem().toString());
 
-        }
-        Log.e("Refresh ", " onItemSelected");
-        listFragmentAdapter.notifyDataSetChanged();
-    }
+        categoryFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final AdapterView<?> adapterView, View view, int i, long l) {
+                categoryFilterText = adapterView.getSelectedItem().toString();
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+                //Filter By Category
+                if (adapterView.getSelectedItem().toString().equals("Category...")) {
+                    categoryFilterText = "";
+                    filteredTicketArrayList = filter(ticketArrayList, categoryFilterText, filterText);
+                    Log.e("Category Select ", "Get all category data");
+                }else{
+                    filteredTicketArrayList = filter(ticketArrayList, categoryFilterText, filterText);
+                    Log.e("Category Select ", adapterView.getSelectedItem().toString());
+                }
+
+                listFragmentAdapter = new ListFragmentAdapter(getContext(), filteredTicketArrayList);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(listFragmentAdapter);
+                listFragmentAdapter.notifyDataSetChanged();
+
+                //Update Ticket
+                listFragmentAdapter.setOnItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void OnItemClick(int position, Ticket ticket) {
+
+                        builder = new AlertDialog.Builder(getContext());
+                        builder.setTitle("Update User Info");
+                        builder.setCancelable(false);
+                        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_list_dialog_update,null,false);
+                        InitUpdateDialog(ticket,position,view);
+                        builder.setView(view);
+                        dialog = builder.create();
+                        dialog.show();
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        et_filterText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filterText = et_filterText.getText().toString();
+                filteredTicketArrayList = filter(ticketArrayList, categoryFilterText, filterText);
+
+                listFragmentAdapter = new ListFragmentAdapter(getContext(), filteredTicketArrayList);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(listFragmentAdapter);
+                listFragmentAdapter.notifyDataSetChanged();
+
+                //Update Ticket
+                listFragmentAdapter.setOnItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void OnItemClick(int position, Ticket ticket) {
+
+                        builder = new AlertDialog.Builder(getContext());
+                        builder.setTitle("Update Ticket Info");
+                        builder.setCancelable(false);
+                        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_list_dialog_update,null,false);
+                        InitUpdateDialog(ticket, position, view);
+                        builder.setView(view);
+                        dialog = builder.create();
+                        dialog.show();
+                    }
+                });
+            }
+        });
 
     }
 
@@ -145,7 +219,7 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
         Cursor res = db.getAllCategoriesData();
 
         categoriesList.clear();
-        categoriesList.add("Filter by category ???");
+        categoriesList.add("Category...");
         while (res.moveToNext()){
             categoriesList.add(res.getString(1));
         }
@@ -159,19 +233,105 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
         startActivity(intent);
     }
 
-    //Filter by category
-    private ArrayList<Ticket> filterByCategory(String categorySelected){
-        ArrayList<Ticket> filteredTicketArrayList = new ArrayList<>();
+    private void InitUpdateDialog(final Ticket ticket, final int position, View view) {
 
-        for (int i=0; i<ticketArrayList.size(); i++){
-            if(ticketArrayList.get(i).getCategory().equals(categorySelected)){
-                filteredTicketArrayList.add(ticketArrayList.get(i));
-                Log.e("Find ", ticketArrayList.get(i).getCategory());
+        final EditText et_update_name = (EditText) view.findViewById(R.id.fragment_list_dialog_update_et_update_name);
+        final EditText et_update_currency = (EditText) view.findViewById(R.id.fragment_list_dialog_update_et_update_currency);
+        Spinner sp_update_category = (Spinner) view.findViewById(R.id.fragment_list_dialog_update_sp_update_category);
+        Button btn_update = (Button) view.findViewById(R.id.fragment_list_dialog_update_btn_update_ticket);
+        Button btn_cancel = (Button) view.findViewById(R.id.fragment_list_dialog_update_btn_update_cancel);
+
+        et_update_name.setText(ticket.getName());
+        et_update_currency.setText(String.valueOf(ticket.getCurrency()));
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, categoriesList);
+        sp_update_category.setAdapter(adapter);
+
+        for (int i=0; i< categoriesList.size(); i++){
+            if (ticket.getCategory().equals(categoriesList.get(i))){
+                sp_update_category.setSelection(i);
             }
         }
 
-        return filteredTicketArrayList;
+        sp_update_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ticket.setCategory(adapterView.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name;
+                double currency;
+
+                name = et_update_name.getText().toString();
+                currency = Double.parseDouble(et_update_currency.getText().toString());
+
+                Ticket ticket1;
+
+                ticket1 = ticket;
+                ticket1.setName(name);
+                ticket1.setCategory(ticket.getCategory());
+                ticket1.setCurrency(currency);
+
+                listFragmentAdapter.UpdateData(position,ticket1);
+                Toast.makeText(getContext(),"User Updated..",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
     }
 
+    private ArrayList<Ticket> filter(ArrayList<Ticket> list, String category, String text){
+        ArrayList<Ticket> filteredTicketArrayList = new ArrayList<>();
+
+        //if both filter arguments are empty
+        if (category.equals("") && text.equals("")){
+            filteredTicketArrayList = list;
+        }else{
+            for (int i=0; i<list.size(); i++){
+
+                //Filter by category
+                if(!category.equals("") && text.equals("")){
+                    if (list.get(i).getCategory().equals(category)){
+                        filteredTicketArrayList.add(list.get(i));
+                        Log.e("Find by Category: "+i+" ==> ", "Category |==> "+list.get(i).getCategory());
+                    }
+                }
+
+                //Filter by text
+                if (!text.equals("") && category.equals("")){
+                    if (list.get(i).getName().contains(text) || list.get(i).getDate().contains(text)){
+                        filteredTicketArrayList.add(list.get(i));
+                        Log.e("Find by String : "+i+" ==> ", "String search: "+text+" |==> Name : "+list.get(i).getName()+" | Date :"+list.get(i).getDate());
+                    }
+                }
+
+                //Filter by category and text
+                if (!category.equals("") && !text.equals("")){
+                    if (list.get(i).getCategory().equals(category) && (list.get(i).getName().contains(text) || list.get(i).getDate().contains(text))){
+                        filteredTicketArrayList.add(list.get(i));
+                        Log.e("Find by Category & Text", " "+i+" ==>\n" +
+                                "Category |==> "+list.get(i).getCategory()+"\n" +
+                                "Name |==> "+list.get(i).getName()+"\n" +
+                                "Date |==> "+list.get(i).getDate());
+                    }
+                }
+            }
+        }
+        return filteredTicketArrayList;
+    }
 
 }
