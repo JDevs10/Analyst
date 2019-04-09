e1a80bcbef78f3bcba01fa888d56f72a481283bf
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-04-09 23:06:46 +0200

    init/test GraphView Line graph

--- app/src/main/java/com/example/fragmenttest/Fragments/GraphFragment.java
+++ app/src/main/java/com/example/fragmenttest/Fragments/GraphFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragmenttest.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,13 +11,49 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fragmenttest.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class GraphFragment extends Fragment {
+
+    private View v;
+    private Context mContext;
+    private String TAG = ListFragment.class.getSimpleName();
+    private GraphView graph;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.e("Testing :", this.getClass().getSimpleName()+" is enable !!!!!!!!!");
-        return inflater.inflate(R.layout.fragment_graph, container,false);
+        v = inflater.inflate(R.layout.fragment_graph, container,false);;
+
+        graph = (GraphView) v.findViewById(R.id.graph);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0.0, 1.1),
+                new DataPoint(1.0, -5.4),
+                new DataPoint(2.5, 3.9),
+                new DataPoint(3.3, -2.4),
+                new DataPoint(4.2, 6.7)
+        });
+        graph.addSeries(series);
     }
 }
