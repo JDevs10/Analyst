e7beae7ec79ea74f163867d29e7a9784aa2b60df
JDevs10 <j-ldsxm18@hotmail.com>
2019-04-10 11:30:20 +0200

    Add && tested MPAndroid library for my future graphs, charts

--- app/src/main/java/com/example/fragmenttest/Fragments/GraphFragment.java
+++ app/src/main/java/com/example/fragmenttest/Fragments/GraphFragment.java
@@ -1,26 +1,30 @@
 package com.example.fragmenttest.Fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fragmenttest.R;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.data.Entry;
+import java.util.ArrayList;
 
 public class GraphFragment extends Fragment {
 
     private View v;
     private Context mContext;
     private String TAG = ListFragment.class.getSimpleName();
-    private GraphView graph;
+
+    private LineChart graph;
 
     @Override
     public void onAttach(Context context) {
@@ -38,7 +42,7 @@ public class GraphFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_graph, container,false);;
 
-        graph = (GraphView) v.findViewById(R.id.graph);
+        graph = (LineChart) v.findViewById(R.id.lineChart);
 
         return v;
     }
@@ -47,13 +51,34 @@ public class GraphFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0.0, 1.1),
-                new DataPoint(1.0, -5.4),
-                new DataPoint(2.5, 3.9),
-                new DataPoint(3.3, -2.4),
-                new DataPoint(4.2, 6.7)
-        });
-        graph.addSeries(series);
+        graph.setDragEnabled(true);
+        graph.setScaleEnabled(false);
+
+        //add chart point values
+        ArrayList<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 45f));
+        yValues.add(new Entry(1, 20f));
+        yValues.add(new Entry(2, 90f));
+        yValues.add(new Entry(3, 10f));
+        yValues.add(new Entry(4, 20f));
+        yValues.add(new Entry(5, 5f));
+
+
+        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+        set1.setFillAlpha(110);
+
+        //Customize Chart
+        set1.setColor(Color.BLUE);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(10f);
+        set1.setValueTextColor(Color.WHITE);
+
+        //Add the line data into an arrayList of line Chart
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        //Display the Chart
+        LineData data = new LineData(dataSets);
+        graph.setData(data);
     }
 }
