f3d86879e229d9b7fcc92605b267ccc6ca02de36
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-04-19 23:49:30 +0200

    Global diagram Pie Chart

--- app/src/main/java/com/example/fragmenttest/Fragments/DiagramCircleFragment.java
+++ app/src/main/java/com/example/fragmenttest/Fragments/DiagramCircleFragment.java
@@ -1,6 +1,7 @@
 package com.example.fragmenttest.Fragments;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,9 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.fragmenttest.Database.DatabaseHelper;
 import com.example.fragmenttest.R;
+import com.example.fragmenttest.objects.DiagramValues;
+import com.example.fragmenttest.objects.Ticket;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
@@ -21,9 +26,10 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-
 
 public class DiagramCircleFragment extends Fragment {
     private String TAG = DiagramCircleFragment.class.getSimpleName();
@@ -32,6 +38,7 @@ public class DiagramCircleFragment extends Fragment {
     private Context mContext;
 
     private PieChart pieChart;
+    private ArrayList<DiagramValues> diagramValues;
     private float[] yData = {1.6f, 10.4f, 60.0f, 10.26f, 26.10f, 75.0f, 99.9f};
     private String[] xData = {"test1", "test2", "test3", "test4", "test5", "test6", "test7",};
 
@@ -60,18 +67,19 @@ public class DiagramCircleFragment extends Fragment {
 
         //"Global Debit Pie Chart by Categories"
         pieChart.getDescription().setText("Global Pie Chart by Categories");
-        pieChart.getDescription().setTextSize(16f);
+        pieChart.getDescription().setTextSize(14f);
 
         pieChart.setNoDataText("No Data Yet");
         pieChart.setRotationEnabled(true);
-        pieChart.setHoleRadius(30f);
+        pieChart.setHoleRadius(50f);
         pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Debit Chart");
+        pieChart.setCenterText("Global Chart");
         pieChart.setCenterTextColor(Color.BLACK);
         pieChart.setCenterTextSize(16);
         pieChart.setHoleColor(Color.TRANSPARENT);
-
-        //pieChart.setUsePercentValues(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setDragDecelerationFrictionCoef(0.99f);
+        pieChart.animateY(1000, Easing.EaseInOutCubic);
 
         return v;
     }
@@ -80,14 +88,17 @@ public class DiagramCircleFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        getDataFromDb();
         addDatatToChart();
 
         pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
             public void onValueSelected(Entry e, Highlight h) {
                 Log.e(TAG, " onValueSelected: Value selected from chart");
-                Log.e(TAG, " onValueSelected: Y value => "+e.toString());
-                Log.e(TAG, " onValueSelected: X value => "+h.toString());            }
+                Log.e(TAG, " onValueSelected: X value => "+h.getX()+" Y value => "+h.getY());
+                Toast.makeText(mContext, "Category: "+diagramValues.get((int) h.getX()).getCategoryName()+" \n" +
+                        "Percentage: "+h.getY()+" %", Toast.LENGTH_LONG).show();
+            }
 
             @Override
             public void onNothingSelected() {
@@ -96,35 +107,81 @@ public class DiagramCircleFragment extends Fragment {
         });
     }
 
+    private void getDataFromDb(){
+        Log.e(TAG, " getDataFromDb");
+
+        //1 get all the categories in the table
+        Cursor res1 = db.getAllCategoriesData();
+        ArrayList<String> categoryNames = new ArrayList<>();
+        while (res1.moveToNext()){
+            categoryNames.add(res1.getString(1));
+            Log.e(TAG, " categoryNames : "+res1.getString(1));
+        }
+
+
+        //2 get all tickets of each category and calculate the sum of all values in the category
+        ArrayList<Double> totalValueOfEachCategory = new ArrayList<>();
+
+        //in each category (found in the db table) get all the ticket currency and calculate the total value
+        for (int i=0; i<categoryNames.size(); i++){
+            double categoryTotalValue = 0.0;
+
+            Cursor res2 = db.getAllTicketData();
+            while (res2.moveToNext()){
+                //Log.e(TAG, " condition if : "+categoryNames.get(i)+" == "+res2.getString(2));
+                if (categoryNames.get(i).equals(res2.getString(2))){
+
+                    //if the ticket type is credit or debit type
+                    if (res2.getString(3).equals("+")){
+                        categoryTotalValue = categoryTotalValue + res2.getDouble(4);
+                    }
+                    //if the ticket type is credit or debit type
+                    if (res2.getString(3).equals("-")){
+                        categoryTotalValue = categoryTotalValue - res2.getDouble(4);
+                    }
+                    //Log.e(TAG, " categoryTotalValue: "+categoryTotalValue);
+                }
+            }
+            Log.e(TAG, " categoryTotalValue done: "+categoryTotalValue);
+            totalValueOfEachCategory.add(categoryTotalValue);
+        }
+
+
+        //3 calculate the global value of all category
+        double globalValueOfAllCategories = 0.0;
+        for (int i=0; i<totalValueOfEachCategory.size(); i++){
+            globalValueOfAllCategories += totalValueOfEachCategory.get(i);
+        }
+        Log.e(TAG, " globalValueOfAllCategories : "+globalValueOfAllCategories);
+
+        //4 calculate the percentage of the category from the global value
+        ArrayList<Float> categoryValues = new ArrayList<>();
+        for (int i=0; i<totalValueOfEachCategory.size(); i++){
+           categoryValues.add( (float) ((totalValueOfEachCategory.get(i) / globalValueOfAllCategories) * 100) );
+           Log.e(TAG, " percentage : "+ (float) ((totalValueOfEachCategory.get(i) / globalValueOfAllCategories) * 100));
+        }
+
+        //populate the diagram values
+        diagramValues = new ArrayList<>();
+        for (int i=0; i<categoryNames.size(); i++){
+            diagramValues.add(new DiagramValues(categoryNames.get(i), categoryValues.get(i)));
+        }
+
+    }
+
     private void addDatatToChart(){
         Log.e(TAG, " addDatatToChart !");
         ArrayList<PieEntry> yEntry = new ArrayList<>();
-        ArrayList<String> xEntry = new ArrayList<>();
 
-        for (int i =0; i<yData.length; i++){
-            yEntry.add(new PieEntry(yData[i], i));
-        }
-
-        for (int i =0; i<xData.length; i++){
-            xEntry.add(xData[i]);
+        for (int i =0; i<diagramValues.size(); i++){
+            yEntry.add(new PieEntry(diagramValues.get(i).getTotalCategoryValue(), diagramValues.get(i).getCategoryName()));
         }
 
         //create data list
         PieDataSet pieDataSet = new PieDataSet(yEntry, "Categories");
         pieDataSet.setSliceSpace(2);
         pieDataSet.setValueTextSize(14);
-
-        //add colors
-        ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.BLUE);
-        colors.add(Color.RED);
-        colors.add(Color.GREEN);
-        colors.add(Color.YELLOW);
-        colors.add(Color.MAGENTA);
-        colors.add(Color.CYAN);
-        colors.add(Color.GRAY);
-
-        pieDataSet.setColors(colors);
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
         //add legend to chart
         Legend legend = pieChart.getLegend();
@@ -133,6 +190,8 @@ public class DiagramCircleFragment extends Fragment {
 
         //create pie data object
         PieData pieData = new PieData(pieDataSet);
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.BLACK);
         pieChart.setData(pieData);
         pieChart.invalidate();
 
